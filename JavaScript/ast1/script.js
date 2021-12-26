e71d8a8aa809e3d945288e34fb60456dd7c1e459
Saurav Shrestha <sauravshrestha28@gmail.com>
2021-12-26 18:44:44 +0545

    JavaScript Assignment 1: Carousel

---
+++ JavaScript/ast1/script.js
@@ -0,0 +1,116 @@
+const carouselContainer = document.querySelector('.carousel-container')
+const carouselWrapper = document.querySelector('.carousel-image-wrapper')
+
+const images = document.querySelectorAll('.carousel-image-wrapper img')
+const imageCount = images.length
+const imgWidth = images[0].clientWidth
+
+carouselContainer.style.width = imgWidth + 'px'
+
+carouselWrapper.style.width = `${imageCount * imgWidth}px`
+
+for (let i = 0; i < imageCount; i++) {
+	images[i].style.left = i * imgWidth + 'px'
+}
+
+let index = 0
+let prevIndex = -1
+let shifting = 0
+
+function nextImage() {
+	prevIndex = index
+	index++
+	if (index >= imageCount) {
+		index = 0
+	}
+	let animate = setInterval(() => {
+		shifting += 5
+		if (shifting > imgWidth) {
+			clearInterval(animate)
+			shifting = 0
+		} else {
+			if (prevIndex === imageCount - 1) {
+				carouselWrapper.style.left = `${
+					-prevIndex * imgWidth + (imageCount - 1) * shifting
+				}px`
+			} else {
+				carouselWrapper.style.left = `${
+					-prevIndex * imgWidth - shifting
+				}px`
+			}
+		}
+	}, 5)
+}
+
+function previousImage() {
+	prevIndex = index
+	index--
+	if (index < 0) {
+		index = imageCount - 1
+	}
+	let animate = setInterval(() => {
+		shifting += 5
+		if (shifting > imgWidth) {
+			clearInterval(animate)
+			shifting = 0
+		} else {
+			if (prevIndex === 0) {
+				carouselWrapper.style.left = `${
+					-prevIndex * imgWidth - (imageCount - 1) * shifting
+				}px`
+			} else {
+				carouselWrapper.style.left = `${
+					-prevIndex * imgWidth + shifting
+				}px`
+			}
+		}
+	}, 5)
+}
+
+const leftArrow = document.createElement('button')
+leftArrow.setAttribute('class', 'left-arrow')
+carouselContainer.appendChild(leftArrow)
+
+const rightArrow = document.createElement('button')
+rightArrow.setAttribute('class', 'right-arrow')
+carouselContainer.appendChild(rightArrow)
+
+rightArrow.onclick = nextImage
+leftArrow.onclick = previousImage
+
+const imgCounter = []
+const imgCounterContainer = document.createElement('div')
+imgCounterContainer.setAttribute('class', 'img-counter-container')
+carouselContainer.appendChild(imgCounterContainer)
+
+for (let i = 0; i < imageCount; i++) {
+	const counter = document.createElement('div')
+	imgCounter.push(counter)
+	counter.setAttribute('class', 'counter')
+	imgCounterContainer.appendChild(counter)
+
+	counter.onclick = function () {
+		if (index !== i) {
+			prevIndex = index
+			index = i
+			let animate = setInterval(() => {
+				shifting += 5
+				if (shifting > imgWidth) {
+					clearInterval(animate)
+					shifting = 0
+				} else {
+					carouselWrapper.style.left = `${
+						-prevIndex * imgWidth - (index - prevIndex) * shifting
+					}px`
+				}
+			}, 5)
+		}
+	}
+}
+
+setInterval(() => {
+	imgCounter[index].style.background = '#49c'
+	if (prevIndex !== -1) {
+		imgCounter[prevIndex].style.background = '#eee1f7'
+	}
+}, 10)
